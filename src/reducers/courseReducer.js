52ca413f0b071eb4e96c4019c986b02bdf275210
Frederick Rouleau <frederick.rouleau@ericsson.com>
2017-05-06 13:25:52 -0400

    After Async Writes in Redux

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,9 +1,26 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function courseReducer(state = [], action) { // Setting initial state to []
+export default function courseReducer(state = initialState.courses, action) {
   switch(action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
+
+    case types.CREATE_COURSE_SUCCESS:
+      // Create a new array with a copy of all existing states plus the new one
+      return [
+        ...state, // ES6 spread operator (explodes all values in the array)
+        Object.assign({}, action.course)
+      ];
+
+    case types.UPDATE_COURSE_SUCCESS:
+      return [
+        // ES6 filter to get a list of all the courses except the course that is being updated
+        // Creates a new array out of the filtered results that are returned
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.course)
+      ];
+
     default:
       return state;
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
-  courses // Short hand prop name (same as saying "courses: courses")
+  courses, // Short hand prop name (same as saying "courses: courses")
+  authors
 });
 
 export default rootReducer;
